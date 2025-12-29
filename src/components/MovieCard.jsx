5eaca49d9968be6cecbef59696664303574eecc8
Vaishhnavi <vaishnavinakhate.here@gmail.com>
2025-12-29 15:09:10 +0530

    Add files via upload

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,28 @@
+
+import '../css/MovieCard.css'
+
+const MovieCard = ({movie}) => {
+    
+    function onFavClick(){
+       
+        alert("clicked")
+    }
+
+  return (
+    <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className="fav-btn" onClick={onFavClick}>♥ </button>
+            </div>
+        </div>
+      
+      <div className='movie-info'>
+        <h3>{movie.title}</h3>
+        <p>{movie.release_data}</p>
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,17 @@
+import '../css/Favorites.css'
+
+
+
+function Favorites() {
+  return (
+    <div className='favorites-empty'>
+        <h2>No fav movie yet </h2>
+        <p>start adding movies to your fav and they will appear hear</p>
+      
+
+     
+    </div>
+  )
+}
+
+export default Favorites
